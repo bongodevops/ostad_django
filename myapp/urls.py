1907b6d26eab52dd379dd42a182d81cc01cc3c00
bongodevops <bongodevops@gmail.com>
2025-04-14 23:29:42 +0600

    template

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
    path('hello/', views.say_hello, name='say_hello'), 
+   path('home/', views.HomePage, name='home'),
+   path('about/', views.AboutPage, name='about'),
+   path('contact/', views.ContactPage, name='contact'),
+   path('services/', views.ServicesPage, name='services'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from django.http import HttpResponse
 
 
 # Create your views here.
 def say_hello(request):
     return HttpResponse("Hello, world!")
+def HomePage(request):
+    return render(request,'home.html')
+def AboutPage(request):
+    return render(request,'about.html')
+def ContactPage(request):
+    return render(request,'contact.html')
+def ServicesPage(request):
+    return render(request,'services.html')
