405723afb605fcc56a720ad1fa892cdd26506988
bongodevops <bongodevops@gmail.com>
2025-04-15 22:53:19 +0600

    Data recive,static,template inheritance

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
@@ -10,6 +10,40 @@ def HomePage(request):
 def AboutPage(request):
     return render(request,'about.html')
 def ContactPage(request):
-    return render(request,'contact.html')
+    
+    profile =[
+        "fb.com/hafizurrahmanomar/",
+        "github.com/hafizurrahmanomar",
+        "linkedin.com/in/hafizurrahmanomar",
+        "instagram.com/hafizurrahmanomar/",
+        "twitter.com/hafizurrahmanomar",
+        "youtube.com/@hafizurrahmanomar",
+        "tiktok.com/@hafizurrahmanomar",
+        "pinterest.com/hafizurrahmanomar",
+        "stackoverflow.com/users/20220623/hafizurrahmanomar",
+        "dribbble.com/hafizurrahmanomar",
+        "behance.net/hafizurrahmanomar",
+        "medium.com/@hafizurrahmanomar",
+        "dev.to/hafizurrahmanomar",
+        "quora.com/profile/Hafizur-Rahman-Omar",
+        "goodreads.com/hafizurrahmanomar",
+        "blogger.com/hafizurrahmanomar",
+        "medium.com/@hafizurrahmanomar",
+    ]
+    
+    return render(request,'contact.html',{"social_links":profile,})
 def ServicesPage(request):
-    return render(request,'services.html')
+    
+    services = {
+        "serve_01": "Web Development",
+        "serve_02": "Web Design",
+        "serve_03": "UI/UX Design",
+        "serve_04": "Graphic Design",
+        "serve_05": "SEO Optimization",
+        "serve_06": "Digital Marketing",
+        "serve_07": "Content Writing",
+        "serve_08": "Social Media Management",
+    }
+
+    context = {'services': services,}
+    return render(request,'services.html',context)
