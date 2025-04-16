7b5a73e51bef2aec683478cf532037b14a151503
bongodevops <bongodevops@gmail.com>
2025-04-16 23:25:32 +0600

    fixture correction

--- todo_app/models.py
+++ todo_app/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+ 
 
     def __str__(self):
         return self.title
